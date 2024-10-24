496e299ab6dd337adf5502bdf42e50b1e9ff8e66
Jello-on-mello <Antoni.l.chmura@gmail.com>
2024-10-24 14:07:54 +0200

    Dodanie wiecej przykładowych danych

--- Lepsze_biuroPodrozy/BiuroPodrozy.java
+++ Lepsze_biuroPodrozy/BiuroPodrozy.java
@@ -117,7 +117,7 @@ public class BiuroPodrozy {
      * Wyświetla listę wycieczek posortowanych według wartości obrotów (od najwyższej).
      */
     public void wycieczkiPosortowanePoObrotach() {
-        // Proste sortowanie bąbelkowe bez użycia Comparatora
+        // Proste sortowanie bąbelkowe
         for (int i = 0; i < liczbaWycieczek - 1; i++) {
             for (int j = 0; j < liczbaWycieczek - i - 1; j++) {
                 if (wykupioneWycieczki[j].getCenaZRabatem() < wykupioneWycieczki[j + 1].getCenaZRabatem()) {
@@ -148,6 +148,7 @@ public class BiuroPodrozy {
 
         System.out.println("\nKlient z najwyższą zapłaconą kwotą:");
         klientZNajwyzszaKwota();
+
         System.out.println("\n-----------------------------------------------------------------\n");
 
         System.out.println("\nWycieczki posortowane według obrotów:");
@@ -167,21 +168,49 @@ public class BiuroPodrozy {
     public static void main(String[] args) {
         BiuroPodrozy biuro = new BiuroPodrozy();
 
+        // Klienci
         Klient klient1 = new Klient("Jan", "Kowalski", 30, "Warszawa");
         Klient klient2 = new Klient("Anna", "Nowak", 25, "Kraków");
         Klient klient3 = new Klient("Piotr", "Wiśniewski", 45, "Gdańsk");
+        Klient klient4 = new Klient("Katarzyna", "Zielińska", 28, "Wrocław");
+        Klient klient5 = new Klient("Michał", "Kowalczyk", 35, "Poznań");
+        Klient klient6 = new Klient("Ewa", "Wojciechowska", 40, "Łódź");
+        Klient klient7 = new Klient("Tomasz", "Kaczmarek", 32, "Szczecin");
+        Klient klient8 = new Klient("Zofia", "Krawczyk", 29, "Lublin");
+        Klient klient9 = new Klient("Jakub", "Szymański", 36, "Katowice");
 
+        // Wycieczki
         Wycieczka wycieczka1 = new Wycieczka("Egipt", 3000, 5);
         Wycieczka wycieczka2 = new Wycieczka("Hiszpania", 2500, 3);
         Wycieczka wycieczka3 = new Wycieczka("Włochy", 3200, 2);
+        Wycieczka wycieczka4 = new Wycieczka("Grecja", 2800, 7);
+        Wycieczka wycieczka5 = new Wycieczka("Francja", 3500, 4);
+        Wycieczka wycieczka6 = new Wycieczka("Turcja", 2600, 6);
+        Wycieczka wycieczka7 = new Wycieczka("Norwegia", 4000, 10);
+        Wycieczka wycieczka8 = new Wycieczka("Szwajcaria", 4500, 8);
+        Wycieczka wycieczka9 = new Wycieczka("Portugalia", 2700, 5);
 
+        // Daty
         Data data1 = new Data("2024-10-10", "2024-10-17");
         Data data2 = new Data("2024-11-01", "2024-11-08");
         Data data3 = new Data("2024-12-20", "2024-12-27");
+        Data data4 = new Data("2024-05-10", "2024-05-17");
+        Data data5 = new Data("2024-06-01", "2024-06-08");
+        Data data6 = new Data("2024-07-15", "2024-07-22");
+        Data data7 = new Data("2024-08-05", "2024-08-12");
+        Data data8 = new Data("2024-09-15", "2024-09-22");
+        Data data9 = new Data("2024-10-01", "2024-10-08");
 
+        // Dodawanie wykupionych wycieczek do biura
         biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient1, wycieczka1, data1));
-        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient1, wycieczka2, data2));
-        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient2, wycieczka3, data3));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient2, wycieczka2, data2));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient3, wycieczka3, data3));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient4, wycieczka4, data4));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient5, wycieczka5, data5));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient6, wycieczka6, data6));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient7, wycieczka7, data7));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient8, wycieczka8, data8));
+        biuro.dodajWykupionaWycieczke(new WykupionaWycieczka(klient9, wycieczka9, data9));
 
         biuro.generujRaporty();
     }
